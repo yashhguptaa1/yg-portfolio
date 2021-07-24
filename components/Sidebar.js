01aadf41e5c36145344a3eb8e808f174c1a47091
yashhguptaa1 <yashhguptaa1@gmail.com>
2021-07-24 16:34:17 +0530

    Creating sidebar and navbar

---
+++ components/Sidebar.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import Navigation from './Navigation';
+
+function Sidebar() {
+    return (
+        <SidebarStyled>
+            <Navigation/>
+        </SidebarStyled>
+    )
+}
+
+const SidebarStyled = styled.div`
+    width: 16.3rem;
+    position: fixed;
+    height: 100vh;
+    background-color: var(--sidebar-dark-color);
+    overflow: hidden;
+    transition: all .4s ease-in-out;
+    @media screen and (max-width:1200px){
+        transform: translateX(-100%);
+        z-index: 20;
+    }
+`;
+
+export default Sidebar;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import Sidebar from "../components/Sidebar";
+
+import { IconButton } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
 
 const index = () => {
-    
   return (
     <div className="App">
-      WELCOME
+      <Sidebar />
+
+      <div className="ham-burger-menu">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+      </div>
+
     </div>
   );
 };
