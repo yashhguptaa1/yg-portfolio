47ca44bdb0a091fe9eec980675e5b2b5d18fbcff
yashhguptaa1 <yashhguptaa1@gmail.com>
2021-07-24 17:11:56 +0530

    Implemented light-dark-mode theme toggler

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,58 @@
 import { useState } from "react";
+import { useEffect } from "react";
 
 import Sidebar from "../components/Sidebar";
 
 import { IconButton } from "@material-ui/core";
-import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from "@material-ui/icons/Menu";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import Switch from "@material-ui/core/Switch";
 
 const index = () => {
+  const [theme, setTheme] = useState("dark-theme");
+  const [checked, setChecked] = useState(false);
+  const [navToggle, setNavToggle] = useState(false);
 
-    const [navToggle, setNavToggle] = useState(false);
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+
+  const themeToggler = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+      setChecked(false);
+    } else {
+      setTheme("light-theme");
+      setChecked(true);
+    }
+  };
 
   return (
     <div className="App">
       <Sidebar navToggle={navToggle} />
+
+      <div className="theme">
+        <div className="light-dark-mode">
+          <div className="left-content">
+            <Brightness4Icon />
+          </div>
+          <div className="right-content">
+            <Switch
+              value=""
+              checked={checked}
+              inputProps={{ "aria-label": "" }}
+              size="medium"
+              onClick={themeToggler}
+            />
+          </div>
+        </div>
+      </div>
 
       <div className="ham-burger-menu">
         <IconButton onClick={() => setNavToggle(!navToggle)}>
           <MenuIcon />
         </IconButton>
       </div>
-
     </div>
   );
 };
