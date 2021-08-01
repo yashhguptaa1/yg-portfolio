890b494db440f98e1f3e8f115755ede5d71bf5c8
Yash Gupta <yashhguptaa1@gmail.com>
2021-08-01 08:14:43 +0530

    Filter to change view between list and card view

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,51 @@
+import { useState } from "react";
+
 import { Row, Col } from "react-bootstrap";
 import PageLayout from "../components/PageLayout";
 import AuthorIntro from "../components/AuthorIntro";
 import CardItem from "../components/CardItem";
+import CardListItem from "../components/CardListItem";
+import FilteringMenu from "../components/FilteringMenu";
 
 import { getAllBlogs } from "../lib/api";
 
 function Home({ blogs }) {
-  debugger;
+  //changing view between card and list
+  const [filter, setFilter] = useState({
+    view: { list: 1 },
+  });
 
   return (
     <PageLayout>
       <AuthorIntro />
+      <FilteringMenu
+        onChange={() => {
+          alert("Alert from index");
+        }}
+      />
       <hr />
       <Row className="mb-5">
-        {/* <Col md="10">
-          <CardListItem />
-        </Col>
-        <Col md="4">
-          <CardItem />
-        </Col> */}
-        {blogs.map((blog) => (
-          <Col key={blog.slug} md="4">
-            <CardItem
-              author={blog.author}
-              title={blog.title}
-              subtitle={blog.subtitle}
-              date={blog.date}
-              image={blog.coverImage}
-              slug={blog.slug}
-              link={{
-                href: "/blogs/[slug]", //file path
-                as: `/blogs/${blog.slug}`, //to be displayed url
-              }}
-            />
-          </Col>
-        ))}
+        {blogs.map((blog) =>
+          filter.view.list === 1 ? (
+            <Col md="9">
+              <CardListItem />
+            </Col>
+          ) : (
+            <Col key={blog.slug} md="4">
+              <CardItem
+                author={blog.author}
+                title={blog.title}
+                subtitle={blog.subtitle}
+                date={blog.date}
+                image={blog.coverImage}
+                link={{
+                  href: "/blogs/[slug]",
+                  as: `/blogs/${blog.slug}`,
+                }}
+              />
+            </Col>
+          )
+        )}
       </Row>
     </PageLayout>
   );
